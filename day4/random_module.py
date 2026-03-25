021012e64e92fcb093b72082614cca3c47ade3c4
raihan <raihanizharul7@gmail.com>
2026-03-25 07:19:52 +0700

    upload random_module.py script

---
+++ day4/random_module.py
@@ -0,0 +1,18 @@
+import random
+
+#random_int=random.randint(1,10)
+#print(random_int)
+
+#random_0_to_1=random.random()
+#print(random_0_to_1)
+
+#random_float=random.uniform(1,10)
+#print(random_float)
+
+random_head_or_tails=random.randint(1,2)
+print(random_head_or_tails)
+if random_head_or_tails==1:
+    print("Head")
+elif random_head_or_tails==2:
+    print("Tails")
+
