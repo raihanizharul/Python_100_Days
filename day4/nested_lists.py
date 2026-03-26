1f0eca0ee64060eebd63b52d83b01efd5dff07ed
raihan <raihanizharul7@gmail.com>
2026-03-26 16:19:26 +0700

    upload nested_lists.py script

---
+++ day4/nested_lists.py
@@ -0,0 +1,20 @@
+fruits = [
+    "strawberries",
+    "nectarines",
+    "apples",
+    "grapes",
+    "peaches",
+    "cherries",
+    "pears"
+]
+
+vegetables = [
+    "spinach",
+    "kale",
+    "tomatoes",
+    "celery",
+    "potatoes"
+]
+
+dirty_dozen=(fruits,vegetables)
+print(dirty_dozen)
