ae382966095b320406d7d0835055e99b0b453dbd
raihan <raihanizharul7@gmail.com>
2026-03-26 15:58:08 +0700

    upload lists_lessons.py script

---
+++ day4/lists_lessons.py
@@ -0,0 +1,15 @@
+province_of_indonesia=[
+    "Jawa Barat",
+    "Jawa Tengah",
+    "Jawa Timur",
+    "Sumatera Utara",
+    "Bali"
+]
+
+first_province_of_indonesia=province_of_indonesia[0]
+print(first_province_of_indonesia)
+
+#add multiple value in list 
+province_of_indonesia.extend(["Riau","Jambi"])
+
+print(province_of_indonesia)
